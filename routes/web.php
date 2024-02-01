4cef32a8b68f734fa20c1d6b8bedd74951fba797
unknown <favourgabriel50@gmail.com>
2024-02-01 04:46:48 +0100

    completed the home section and working on the about section

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', [PortfolioController::class, 'porfolio'])->name('portfolio');
+Route::get('/', [PortfolioController::class, 'portfolio'])->name('portfolio');
